2fe3f0470172cc2ea6d4049a5c45a9769afb60ef
Michelle <michelle.jones.2161@hotmail.co.uk>
2025-03-06 12:50:40 +0000

    chore: add settings for deployment

--- denim_whisperer/settings.py
+++ denim_whisperer/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1',
+                 'localhost',
+                 'denim-whisperer-1bbd25e18a83.herokuapp.com']
 
 
 # Application definition
