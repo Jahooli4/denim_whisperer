b3548e0b54f1f332c386861338f615adb13aee83
Michelle <michelle.jones.2161@hotmail.co.uk>
2025-03-18 17:28:57 +0000

    Feat: Set up delivery logic and contexts for bag

--- denim_whisperer/settings.py
+++ denim_whisperer/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -163,3 +164,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
